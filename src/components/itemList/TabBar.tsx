17b95d982dd03130086f57d7f369ad9f9b991045
JJy <darlian0889@gmail.com>
2025-04-29 15:07:30 +0900

    Feat: 커스텀 하단 tab 추가

---
+++ src/components/itemList/TabBar.tsx
@@ -0,0 +1,70 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { itemList } from "@/styles/components/itemList";
+import Home from "@/assets/images/home.svg";
+import { ReactElement } from "react";
+import Colors from "@/constants/Colors";
+
+const TabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => {
+    const primaryColor = Colors.primary;
+    const secondaryColor = Colors.secondary;
+    const whiteColor = Colors.white;
+
+    const icons: Record<string, (props: any) => ReactElement> = {
+        itemList: (props: ReactElement) => <Home color={whiteColor} {...props} />
+        // history: (props: ReactElement) => <Home color={"#ccc"} {...props} />
+    }
+    
+    return (
+        <View style={itemList.tabBar}>
+            {state.routes.map((route, index) => {
+            const { options } = descriptors[route.key];
+            const label =
+                options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                    ? options.title
+                    : route.name;
+
+        if(["_sitemap", "+not-found"].includes(route.name)) return null;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+        const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+        });
+
+        if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+        }
+        };
+      
+        return (
+            <TouchableOpacity
+                key={route.name}
+                style={[itemList.tabBarItem, {backgroundColor: isFocused ? primaryColor : secondaryColor}] }
+                accessibilityRole="button"
+                accessibilityState={isFocused ? { selected: true } : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarButtonTestID}
+                onPress={onPress}
+            >
+                {
+                    icons[route.name]({
+                        color: isFocused? whiteColor : primaryColor,
+                    })
+                }
+                <Text style={{ color: isFocused ? whiteColor : primaryColor }}>
+                    {`${label}`}
+                </Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  }
+
+export default TabBar;
