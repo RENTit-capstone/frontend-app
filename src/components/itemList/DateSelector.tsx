94b64f379f47b30a028cca5acdc69eba0025c389
JJy <darlian0889@gmail.com>
2025-05-10 12:05:42 +0900

    Feat: 커스텀 캘린더 헤더 추가

--- src/components/itemList/DateSelector.tsx
+++ src/components/itemList/DateSelector.tsx
@@ -1,11 +1,54 @@
-import { Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Calendar, LocaleConfig } from "react-native-calendars";
 
 const DateSelector = () => {
+    const [selected, setSelected] = useState('');
+    const [currentMonth, setCurrentMonth] = useState(new Date().toISOString().split('T')[0]);
+    const customHeaderProps: any = useRef('');
+
+    const setCustomHeaderNewMonth = (next = false) => {
+        const add = next ? 1 : -1;
+        const month = new Date(customHeaderProps?.current?.month);
+        const newMonth = new Date(month.setMonth(month.getMonth() + add));
+        customHeaderProps?.current?.addMonth(add);
+        setCurrentMonth(newMonth.toISOString().split('T')[0]);
+    };
+    const moveNext = () => {
+        setCustomHeaderNewMonth(true);
+    };
+    const movePrevious = () => {
+        setCustomHeaderNewMonth(false);
+    };
+
+    const CustomHeader = React.forwardRef((props, ref) => {
+        customHeaderProps.current = props;
+        return (
+            <View>
+                <TouchableOpacity onPress={movePrevious}>
+                    <Text>Previous</Text>
+                </TouchableOpacity>
+                <Text>Custom header!</Text>
+                <Text>{currentMonth}</Text>
+                <TouchableOpacity onPress={moveNext}>
+                    <Text>Next</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    });
+
     return (   
-        <View>
-            <Text>
-                일정 선택
-            </Text>
+        <View> 
+            <Calendar
+                // customHeader={CustomHeader}
+                onDayPress={(day) => {
+                    setSelected(day.dateString);
+                    console.log("selected day", day.dateString);
+                }}
+                markedDates={{
+                    [selected]: {selected: true, disableTouchEvent: true}
+                }}
+            />
         </View>
     );
 }
