cd2be1cb4a789b3c698b0a7c33a6e6060585aeea
JJy <darlian0889@gmail.com>
2025-05-12 00:16:03 +0900

    Refactor: app/ 디렉토리 구조 통일

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,41 +1,22 @@
 import { Tabs } from "expo-router";
-import { View, Pressable } from "react-native";
-import { Common } from "@/styles/common";
 import TabBar from "@/components/bottomTabBar/TabBar";
-import { ReactElement } from "react";
-import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
-import Header from "@/components/Header";
 
 const TabLayout = () => {
     return (
         <Tabs
-            screenOptions={{ headerShown: false }}
+            screenOptions={{headerShown: false}}
             tabBar={(props) => <TabBar {...props} />}
-            // screenOptions={{
-            //     headerShown: true, 
-            //     tabBarActivateTintColor: "#455464",
-            //     tabBarInactiveTintColor: "#ccc",
-            //     tabBarStyle: {
-            //         backgroundColor: Colors.secondary
-            //     },
-            //     tabBarLabelStyle: {
-            //         fontSize: 14,
-            //     }
-            // }}
         >
             <Tabs.Screen
                 name="itemList"
                 options={{
                     title: "홈",
                 }}
-
             />
             <Tabs.Screen
                 name="history"
                 options={{
                     title: "히스토리",
-                    header: () => <Header />,
-
                 }}
             />
         </Tabs>
