47c7985c7a3eb168f7c319b64ae16f96f4ac5bad
JJy <darlian0889@gmail.com>
2025-05-13 12:13:59 +0900

    Feat: 스플래시 스크린 추가

---
+++ src/app/splash.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options (optional)
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Preload fonts or make necessary API calls
+        await Font.loadAsync(Entypo.font);
+        // Simulate a delay for demonstration purposes (remove this in production)
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>RENTit</Text>
+      <Entypo name="rocket" size={30} />
+    </View>
+  );
+}
