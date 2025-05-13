afcc2f284d50dc0068fcdd6dd6c7df98dc114ba9
JJy <darlian0889@gmail.com>
2025-05-13 17:01:31 +0900

    #13 Design: 디자인시스템 추가

---
+++ src/styles/tokens.ts
@@ -0,0 +1,30 @@
+export const Colors = {
+  primary: "#455464",
+  secondary: "#F6EDE0",
+  background: "#FBF5ED",
+  option: "#D4D4D8",
+  error: "#B84C4C",
+
+  white: "#FFFFFF",    
+  black: "#111111",
+  gray: "#767676",
+  darkGray: "#505050",
+  navy: "#3F5466",
+  red: "#ED442D",
+  blue: "#0038FF",
+  brown: "#9F5A00",
+};
+
+export const Spacing = {
+  xs: 2,    
+  sm: 4,
+  md: 12,
+  lg: 16,
+  xl: 32,
+};
+
+export const Radius = {
+  sm: 4,
+  md: 8,
+  lg: 50,
+};
