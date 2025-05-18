a63578993ee558b738626e11e5b49560037e13f7
JJy <darlian0889@gmail.com>
2025-05-18 21:30:54 +0900

    Feat: useDateSelectorStore 추가

---
+++ src/stores/useDateSelectorStore.tsx
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+
+type DateSelectorState = {
+    visible: boolean,
+    startDate: Date | null,
+    endDate: Date | null,
+    resolve?: (result: {startDate: Date | null; endDate: Date | null}) => void;
+
+    openDateSelector: () => Promise<{ startDate: Date | null; endDate: Date | null}>;
+    closeDateSelector: (close: boolean) => void;
+    setStartDate: (date: Date) => void;
+    setEndDate: (date: Date) => void;
+}
+
+const useDateSelectorStore = create<DateSelectorState>(
+    (set, get) => ({
+        visible: false,
+        startDate: null,
+        endDate: null,
+
+        openDateSelector: () => {
+            return new Promise((resolve) => {
+                set({ visible: true, resolve });
+            });
+        },
+        closeDateSelector: (close) => {
+            const { startDate, endDate, resolve } = get();
+            if (close && resolve) {
+                resolve({ startDate, endDate });
+            }
+            else if (resolve) {
+                resolve({ startDate: null, endDate: null });
+            }
+            set({ visible: false, startDate: null, endDate: null, resolve: undefined });
+        },
+
+        setStartDate: (date) => set({ startDate: date }),
+        setEndDate: (date) => set({ endDate: date }),
+    })
+)
