fb8b9a5de6e190281a76ee9cec03d2317da82313
JJy <darlian0889@gmail.com>
2025-05-19 15:16:25 +0900

    Feat: PreSignedImage 컴포넌트 생성

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -1,21 +1,36 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
-import { AuthType } from "@/types/types";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import SecureStore from "expo-secure-store";
+
+type AuthType = {
+   accessToken: string | null;
+   refreshToken: string | null;
+   setAccessToken: (accessToken: string) => void;
+   setRefreshToken: (refreshToken: string) => Promise<void>;
+   clearTokens: () => Promise<void>;
+}
 
 const useAuthStore = create<AuthType>()(
    persist(
       (set, get) => ({
-         //defaultValues
-         id: null,
          accessToken: null,
-         setId: (id) => set({id}),
-         setToken: (accessToken) => set({ accessToken }),
-         clearToken: () => set({ accessToken: null }),
+         refreshToken: null,
+         setAccessToken: (accessToken) => set({ accessToken }),
+         setRefreshToken: async (refreshToken) => {
+            await SecureStore.getItemAsync("refreshToken");
+            set({ refreshToken: refreshToken });
+         },
+         clearTokens: async () => {
+            await SecureStore.deleteItemAsync("refreshToken");
+            set({ accessToken: null, refreshToken: null });
+         },
    }),
    {
       name: "accessToken",
+      partialize: (state) => ({ accessToken: state.accessToken }),
       storage: createJSONStorage(() => AsyncStorage),
-   }));
+   },
+));
 
-export default useAuthStore
+export default useAuthStore;
