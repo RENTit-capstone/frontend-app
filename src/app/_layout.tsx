30bcc0c8113b184283eba3d893b64e2ebbbd39af
JJy <darlian0889@gmail.com>
2025-05-19 12:02:45 +0900

    Feat: 로그인 되어있지 않을 시 로그인창으로 리다이렉션

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,68 +2,56 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
 import { Stack, Redirect } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 import useAuthStore from '@/stores/useAuthStore';
 
-// import { useColorScheme } from '@/components/useColorScheme';
+SplashScreen.preventAutoHideAsync();
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-// 앱 시작 시 로딩 대기
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  const [appReady, setAppReady] = useState(false);
+
   useEffect(() => {
     if (error) throw error;
   }, [error]);
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync().then(() => setAppReady(true));
     }
   }, [loaded]);
 
-  if (!loaded) {
+  if (!loaded || !appReady) {
     return null;
   }
 
   return <RootLayoutNav />;
 }
 
-// 실제 화면 렌더링(라우팅 구조 정의)
 function RootLayoutNav() {
-  // const colorScheme = useColorScheme();
   const token = useAuthStore((state) => state.accessToken);
 
-  // if (!token) {
-  //   // 로그인 되어있지 않다면 로그인 화면으로 이동 -> 에러 발생하여 splash screen 구현 이후 재시도
-  //   return <Redirect href={"/login"} />;
-  // }
+  if (!token) {
+    return <Redirect href={"/login"} />;
+  }
 
   return (
-    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        {/* <Stack.Screen name="(auth)/signup" options={{headerShown: false}} /> */}
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="modal" options={{ presentation: 'modal' }} /> */}
       </Stack>
-    // </ThemeProvider>
   );
 }
 
