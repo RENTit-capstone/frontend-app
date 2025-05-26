f5317267e5b2ffca42d94d2c46568fd5eee7751f
JJy <darlian0889@gmail.com>
2025-05-26 09:25:20 +0900

    Feat: 마이페이지 서브페이지 리스트 추가

--- src/app/(tabs)/mypage.tsx
+++ src/app/(tabs)/mypage.tsx
@@ -3,12 +3,19 @@ import { Common } from "@/styles/common";
 import StatusButtonGroup from "@/components/itemList/StatusButtonGroup";
 import { useRouter } from "expo-router";
 
+const options = ["문의 작성", "내 문의 보기", "OTP 발급", "사물함 이용 안내"]
+
 const Mypage = () => {
   const router = useRouter();
 
   return (
     <View style={Common.container}>
         <StatusButtonGroup />
+        {options.map((item) => (
+            <View>
+                <Text>{item}</Text>
+            </View>
+        ))}
     </View>
     );
 }
