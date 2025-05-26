50d2822d9dcf53876128b61e041de569fb4faa2a
JJy <darlian0889@gmail.com>
2025-05-26 09:31:17 +0900

    Feat: 서브페이지 라우팅

--- src/app/(tabs)/mypage.tsx
+++ src/app/(tabs)/mypage.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { Common } from "@/styles/common";
 import StatusButtonGroup from "@/components/itemList/StatusButtonGroup";
 import { useRouter } from "expo-router";
 
-const options = ["문의 작성", "내 문의 보기", "OTP 발급", "사물함 이용 안내"]
+const options = [{
+    category: "문의",
+    contents: [{
+        name: "문의 작성",
+        link: "/myPage/QnA/NewQnA"
+        }, { 
+        name: "내 문의 보기", 
+        link: "/myPage/QnA/MyQnA",
+        }, {
+        name: "OTP 발급", 
+        link: "/myPage/otp", 
+        }, {
+        name: "사물함 이용 안내",
+        link: "/myPage/otp",
+    }]
+}];
 
 const Mypage = () => {
   const router = useRouter();
@@ -13,7 +28,12 @@ const Mypage = () => {
         <StatusButtonGroup />
         {options.map((item) => (
             <View>
-                <Text>{item}</Text>
+                <Text>{item.category}</Text>
+                {item.contents.map((subpage) => (
+                    <Pressable onPress={() => router.push(subpage.link)}>
+                        <Text>{subpage.name}</Text>
+                    </Pressable>
+                ))}
             </View>
         ))}
     </View>
