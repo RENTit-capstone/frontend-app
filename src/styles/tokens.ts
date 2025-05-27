921f7656e9e8c636d509872f1c98d807e12fc2ed
JJy <darlian0889@gmail.com>
2025-05-27 10:27:16 +0900

    #13 Chore: 폰트, 공백 사이즈 체계 추가

--- src/styles/tokens.ts
+++ src/styles/tokens.ts
@@ -1,3 +1,5 @@
+import normalizeSize from "@/utils/normalizeSize";
+
 export const Colors = {
   primary: "#455464",
   secondary: "#F6EDE0",
@@ -12,5 +14,21 @@ export const Colors = {
   navy: "#3F5466",
   red: "#ED442D",
   blue: "#0038FF",
-  brown: "#9F5A00",
+  brown: "#9F5A00"
 };
+
+export const FontSize = {
+  title: normalizeSize(30),
+  subTitle: normalizeSize(22),
+  body: normalizeSize(16),
+  caption: normalizeSize(14),
+  label: normalizeSize(12)
+}
+
+export const Spacing = {
+  0: 0,
+  4: normalizeSize(4),
+  8: normalizeSize(8),
+  12: normalizeSize(12),
+  16: normalizeSize(16)
+}
