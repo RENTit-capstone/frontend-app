4dab51fb6873065840c0a469fc1242cb5ba6aff3
JJy <darlian0889@gmail.com>
2025-05-27 22:59:00 +0900

    #13 Chore: Colors import경로 수정

--- src/styles/components/history.ts
+++ src/styles/components/history.ts
@@ -1,5 +1,5 @@
-import Colors from '@/constants/Colors';
 import { StyleSheet } from 'react-native';
+import { Colors } from '../tokens';
 
 const primaryColor = Colors.primary;
 const secondaryColor = Colors.secondary;
