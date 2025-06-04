da71b1448a6c78503952ff76bd39a7e542d5c3da
JJy <darlian0889@gmail.com>
2025-06-05 00:26:03 +0900

    Feat: 대여 현황 추가

--- src/components/itemList/StatusButtonGroup.tsx
+++ src/components/itemList/StatusButtonGroup.tsx
@@ -1,15 +1,43 @@
+import { axiosGet } from '@/api';
 import Button from '@/components/Button';
 import { Common } from '@/styles/common';
 import { itemList } from '@/styles/components/itemList';
 import { TextThemes } from '@/styles/theme';
-import React, { useState } from 'react';
+import generateUrl from '@/utils/generateUrl';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const options = ['대여 신청', '대여중', '연체'];
-const count = [5, 5, 0];
 const themes = [TextThemes.statusRequest, TextThemes.statusAccepted, TextThemes.statusOverdue];
 
 const StatusButtonGroup = () => {
+    const [statusCounts, setStatusCounts] = useState([0, 0, 0]);
+
+    useEffect(() => {
+        const statuses = [
+            'REQUESTED',
+            ['APPROVED', 'LEFT_IN_LOCKER', 'PICKED_UP', 'RETURNED_TO_LOCKER'],
+        ];
+        const fetchData = async () => {
+            statuses.forEach(async (item, index) => {
+                const params = generateUrl({
+                    statuses: item,
+                    page: 0,
+                    size: 100,
+                    sort: ['requestDate', 'desc'],
+                });
+                const response = await axiosGet(`/api/v1/rentals?${params}`);
+                const count = response.data.content.length;
+                setStatusCounts((prevCounts) => {
+                    const newCounts = [...prevCounts];
+                    newCounts[index] = count;
+                    return newCounts;
+                });
+            });
+        };
+        fetchData();
+    }, []);
+
     const handlePress = (option: string) => {
         return (
             // 각 항목 클릭 시 히스토리로 이동
@@ -32,7 +60,7 @@ const StatusButtonGroup = () => {
                                 <View style={Common.YStack}>
                                     <Text>{option}</Text>
                                     <Text style={[itemList.statusNumber, themes[index]]}>
-                                        {count[index]}
+                                        {statusCounts[index]}
                                     </Text>
                                 </View>
                             </Button>
